dd79a75f4dae7e9cfcdc30d4ced0363650535f03
m2y2m <meysamsayyadtala@gmail.com>
2024-12-05 09:46:51 +0330

    minor changes

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+
 
 # Create your models here.
+class Package(models.Model):
+    title = models.CharField(_("title"), max_length=50)
+    sku = models.CharField(
+        _("stock keeping unit"), max_length=20, validators=[], db_index=True
+    )
+    description = models.TextField(_("description"))
